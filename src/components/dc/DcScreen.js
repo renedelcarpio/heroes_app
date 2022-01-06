ef3da45df1f7f45f4fc33bd0f747ec7d92d08eb7
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-06 18:24:34 -0400

    styles corrections

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,11 +1,18 @@
+import './DcScreen.scss';
+
 import HeroList from '../hero/HeroList';
 
 const DcScreen = () => {
+	const dcImageUrl = 'https://wallpaper.dog/large/17219721.jpg';
+
 	return (
-		<div>
-			<h1>DC Screen</h1>
-			<hr />
-			<HeroList publisher={'DC Comics'} />
+		<div className='dc__container'>
+			<div className='dc__image-container'>
+				<img src={dcImageUrl} alt='DC Logo' className='dc__image' />
+			</div>
+			<div className='dc__cards-container'>
+				<HeroList publisher={'DC Comics'} />
+			</div>
 		</div>
 	);
 };
--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,11 +1,19 @@
+import './MarvelScreen.scss';
+
 import HeroList from '../hero/HeroList';
 
 const MarvelScreen = () => {
+	const marvelImageUrl =
+		'https://thecouch.world/wp-content/uploads/2018/02/Marvel-Comics-Logo-Desktop-Wallpaperconv.jpg';
+
 	return (
-		<div>
-			<h1>Marvel Screen</h1>
-			<hr />
-			<HeroList publisher={'Marvel Comics'} />
+		<div className='marvel__container'>
+			<div className='marvel__image-container'>
+				<img src={marvelImageUrl} alt='DC Logo' className='marvel__image' />
+			</div>
+			<div className='marvel__cards-container'>
+				<HeroList publisher={'Marvel Comics'} />
+			</div>
 		</div>
 	);
 };
