39d9f6d18df6fb94b00d60f8cb21c3679907042c
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-06 16:17:55 -0400

    create hero list

---
+++ src/components/hero/HeroList.js
@@ -0,0 +1,16 @@
+import HeroCard from './HeroCard';
+import { getHeroesByPublisher } from '../../helpers/getHeroesByPublisher';
+
+const HeroList = ({ publisher }) => {
+	const heroes = getHeroesByPublisher(publisher);
+
+	return (
+		<div>
+			{heroes.map((hero) => (
+				<HeroCard key={hero.id} {...hero} />
+			))}
+		</div>
+	);
+};
+
+export default HeroList;
