e73751bfcaf5a61a2c8a02445f9810bb22b58d0a
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-06 16:17:41 -0400

    add hero list to components

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,7 +1,11 @@
+import HeroList from '../hero/HeroList';
+
 const MarvelScreen = () => {
 	return (
 		<div>
 			<h1>Marvel Screen</h1>
+			<hr />
+			<HeroList publisher={'Marvel Comics'} />
 		</div>
 	);
 };
