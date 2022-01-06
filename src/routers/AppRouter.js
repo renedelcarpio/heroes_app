299d7c57972239cd9a914754bc7ab44676c18b51
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-06 14:20:28 -0400

    add dashboard routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import DcScreen from '../components/dc/DcScreen';
+import DashboardRoutes from './DashboardRoutes';
 import LoginScreen from '../components/login/LoginScreen';
-import MarvelScreen from '../components/marvel/MarvelScreen';
-import { Navbar } from '../components/ui/Navbar';
 import React from 'react';
-import SearchScreen from '../components/search/SearchScreen';
 
 const AppRouter = () => {
 	return (
 		<BrowserRouter>
-			<Navbar />
 			<Routes>
-				<Route path='/marvel' element={<MarvelScreen />} />
-				<Route path='/dc' element={<DcScreen />} />
-				<Route path='/search' element={<SearchScreen />} />
 				<Route path='/login' element={<LoginScreen />} />
+				<Route path='/*' element={<DashboardRoutes />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+
+import DcScreen from '../components/dc/DcScreen';
+import HeroScreen from '../components/hero/HeroScreen';
+import MarvelScreen from '../components/marvel/MarvelScreen';
+import { Navbar } from '../components/ui/Navbar';
+import SearchScreen from '../components/search/SearchScreen';
+
+const DashboardRoutes = () => {
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path='marvel' element={<MarvelScreen />} />
+				<Route path='dc' element={<DcScreen />} />
+				<Route path='search' element={<SearchScreen />} />
+				<Route path='hero' element={<HeroScreen />} />
+				<Route path='/' element={<MarvelScreen />} />
+			</Routes>
+		</>
+	);
+};
+
+export default DashboardRoutes;
