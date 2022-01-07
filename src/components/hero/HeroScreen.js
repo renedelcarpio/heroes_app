5e126f1dbbdb3489428823c537d4e9f7f43360be
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-07 09:47:54 -0400

    functinality to show one hero info

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,19 @@
+import { Navigate, useParams } from 'react-router-dom';
+
+import { getHeroById } from '../../helpers/getHeroById';
+
 const HeroScreen = () => {
+	const { heroId } = useParams();
+
+	const hero = getHeroById(heroId);
+
+	if (!hero) {
+		return <Navigate to='/' />;
+	}
+
 	return (
 		<div>
-			<h1>Hero Screen</h1>
+			<h1>{hero.superhero}</h1>
 		</div>
 	);
 };
