fc33f371d29abb116b51e87dce98b1f7abceb029
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-07 16:39:21 -0400

    search feature

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,7 +1,34 @@
+import './SearchScreen.scss';
+
+import { useForm } from '../../hooks/useForm';
+
 const SearchScreen = () => {
+	const [value, handleInputChange] = useForm({
+		searchText: '',
+	});
+
+	const { searchText } = value;
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+		console.log(searchText);
+	};
+
 	return (
-		<div>
-			<h1>Search Screen</h1>
+		<div className='search__container'>
+			<form onSubmit={handleSearch}>
+				<input
+					type='text'
+					placeholder='Search your favorite superhero'
+					className='search__input'
+					name='searchText'
+					value={searchText}
+					onChange={handleInputChange}
+				/>
+				<button className='search__button' type='submit'>
+					Search...
+				</button>
+			</form>
 		</div>
 	);
 };
