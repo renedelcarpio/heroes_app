88ec75889d9335c2961d335cf85eabe635a8cb43
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-07 15:24:51 -0400

    hero screen created

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,19 +1,66 @@
-import { Navigate, useParams } from 'react-router-dom';
+import './HeroScreen.scss';
+
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 
 import { getHeroById } from '../../helpers/getHeroById';
 
 const HeroScreen = () => {
 	const { heroId } = useParams();
+	const navigate = useNavigate();
 
 	const hero = getHeroById(heroId);
+
+	const handleReturn = () => {
+		navigate(-1);
+	};
 
 	if (!hero) {
 		return <Navigate to='/' />;
 	}
 
+	const { id, superhero, alter_ego, publisher, first_appearance, characters } =
+		hero;
+
+	const imagePath = `/assets/${id}.jpg`;
+
 	return (
-		<div>
-			<h1>{hero.superhero}</h1>
+		<div
+			className={
+				publisher === 'DC Comics'
+					? 'hero__container dc'
+					: 'hero__container marvel'
+			}
+		>
+			<div className='hero__card'>
+				<img
+					src={imagePath}
+					alt={superhero}
+					width={400}
+					className='hero__image'
+				/>
+				<div>
+					<h3 className='hero__title'>{superhero}</h3>
+					<ul>
+						<li className='hero__info'>
+							<b>Alter ego: </b>
+							{alter_ego}
+						</li>
+						<li className='hero__info'>
+							<b>Publisher: </b>
+							{publisher}
+						</li>
+						<li className='hero__info'>
+							<b>First appearance: </b>
+							{first_appearance}
+						</li>
+					</ul>
+					<h5 className='hero__characters--title'>Characters</h5>
+					<p className='hero__info'>{characters}</p>
+					<button className='hero__button' onClick={handleReturn}>
+						Return
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
