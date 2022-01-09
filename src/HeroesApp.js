e012efcd66c2c6445a1df87ded71eb360d9e1116
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-09 10:30:56 -0400

    set global state

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,7 +1,26 @@
 import AppRouter from './routers/AppRouter';
+import { AuthContext } from './auth/authContext';
+import { authReducer } from './auth/authReducer';
+import { useReducer } from 'react';
 
 const HeroesApp = () => {
-	return <AppRouter />;
+	const init = () => {
+		return {
+			logged: true,
+			name: 'Pancho temporal',
+		};
+	};
+	const [user, dispatch] = useReducer(authReducer, {}, init);
+	return (
+		<AuthContext.Provider
+			value={{
+				user,
+				dispatch,
+			}}
+		>
+			<AppRouter />
+		</AuthContext.Provider>
+	);
 };
 
 export default HeroesApp;
